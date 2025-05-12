7837d9a299cf5f45a4b9d710936bf0435441e10d
TuftyRairu <craususrhyle@gmail.com>
2025-05-12 22:14:35 +0800

    Home page progress - 30%, added navbar, 2 sections (Banner and Analytics)

---
+++ src/components/Home/BannerSection.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import Link from "next/link";
+import { ReactTyped } from "react-typed";
+
+const typedStrings = [
+    "{Full Stack} Web ^1000& App<br>Developer_",
+    "Quality Assurance<br>Engineer",
+    "Pentester"
+];
+
+export default function BannerSection () {
+    return (
+        <section className="min-y-screen">
+
+            <div className="flex flex-row justify-around items-center rounded-xl max-h-[90vh] p-6 bg-white">
+                <div className="flex justify-center w-full">
+                    <Image src="/ID2.png" alt="Profile" width={200} height={200} className="pentagon object-cover w-48 h-48 rounded-xl" />
+                </div>
+                <div className="flex flex-col gap-4 w-full pr-2">
+                    <h1 className="flex flex-row gap-2 text-lg md:text-lg lg:text-lg text-[#0E1524]">
+                        <p className="text-red-400">{"<span>"}</p>
+                        <p className="text-gray-500">{"Hey, I'm Rhyle"}</p>
+                        <p className="text-red-400">{"</span>"}</p>
+                    </h1>
+                    <ReactTyped
+                        strings={typedStrings}
+                        className="text-3xl md:text-4xl lg:text-5xl font-bold text-[#036E46] h-[17vh]"
+                        typeSpeed={80}
+                        backSpeed={60}
+                        loop
+                    />
+                    <div className="gap-2 flex-row text-lg md:text-lg lg:text-lg text-[#0E1524] pr-3">
+                        <div className="text-gray-500">
+                            <span className="text-red-400">&lt;p&gt;</span>&nbsp;
+                            With expertise in cutting-edge technologies like
+                            <span className="text-red-400"> NodeJS, React, NextJS, and Laravel</span>,
+                            combined with hands-on experience in
+                            <span className="text-red-400"> Cypress, Postman</span>,
+                            and security tools like
+                            <span className="text-red-400"> Burp Suite</span>,
+                            and Terminal based tools (
+                            <span className="text-red-400">nuclei, subfinder</span>, etc.)
+                            I deliver web solutions that are innovative, robust, and secure. &nbsp;
+                            <span className="text-red-400">&lt;/p&gt;</span>
+                        </div>
+                        <div className="flex flex-row gap-1">
+                            <span>... and</span>
+                            <Link href={"/"}>more</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
---
+++ src/components/Home/ProjectsListSection.tsx
@@ -0,0 +1,16 @@
+
+
+export default function ProjectListSection () {
+    return (
+        <section className="min-y-screen">
+            <div>
+                <div>
+
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+        </section>
+    )
+}
