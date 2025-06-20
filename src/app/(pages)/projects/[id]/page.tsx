d62188d478c91adf84bf34559a0a19cfdf8412aa
rairucrausus <89209183+TuftyRairu@users.noreply.github.com>
2025-06-20 14:55:21 +0800

    Feature/project section (#8)
    * changed the approach of seeing each project, also added some info on the experiences section
    
    * fixing error
    
    * fixing error
    
    * fixing error
    
    * fixing error

---
+++ src/app/(pages)/projects/[id]/page.tsx
@@ -0,0 +1,28 @@
+import { projectList } from "@/lib/projects";
+import { notFound } from "next/navigation";
+import Image from "next/image";
+import { use } from "react";
+
+export default function Projects({
+    params,
+}: {
+    params: Promise<{ id: string }>
+}) {
+    const { id } = use(params);
+    const project = projectList.find((p) => p.link === `/projects/${id}`);
+
+    if (!project) return notFound();
+
+    return (
+        <section className="min-y-screen">
+            <div className="flex flex-col gap-5 rounded-xl py-6 px-6 bg-white">
+                <h1 className="font-bold text-2xl">Projects</h1>
+                <div className="flex flex-col gap-2">
+                    <h1>{project.title}</h1>
+                    <Image src={project.src} alt={project.alt} width={100} height={100} />
+                    <p>{project.description}</p>
+                </div>
+            </div>
+        </section>
+    )
+}
