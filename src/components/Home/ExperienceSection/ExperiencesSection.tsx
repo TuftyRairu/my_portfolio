3dd9ca6ce230a9ca98951fa361dd9d4dd51f3c8b
TuftyRairu <craususrhyle@gmail.com>
2025-06-25 16:29:25 +0800

    fixing error

--- src/components/Home/ExperienceSection/ExperiencesSection.tsx
+++ src/components/Home/ExperienceSection/ExperiencesSection.tsx
@@ -1,12 +1,5 @@
 "use client";
 
-// import {
-//     Accordion,
-//     AccordionContent,
-//     AccordionItem,
-//     AccordionTrigger,
-//   } from "@/components/ui/accordion";
-// import Link from "next/link";
 import { experienceList } from "@/lib/experiences";
 import { ExperienceCard } from "./ExperienceCard";
 
