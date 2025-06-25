90ad0a636e98a4e8ca1dbd90ae8f5cc64bff02ea
TuftyRairu <craususrhyle@gmail.com>
2025-06-25 16:32:47 +0800

    fixing error

--- src/components/Home/ExperienceSection/ExperiencesSection.tsx
+++ src/components/Home/ExperienceSection/ExperiencesSection.tsx
@@ -6,7 +6,7 @@ export default function ExperiencesSection() {
         <section id="experiences" className="min-y-screen">
             <div className="flex flex-col gap-6 rounded-xl py-8 px-6 bg-white">
                 <div className="w-full flex flex-col gap-3 items-center">
-                    <p className="text-green-800 font-semibold">What I've Work On Before</p>
+                    <p className="text-green-800 font-semibold">What I&apos;ve Work On Before</p>
                     <h1 className="text-3xl font-semibold">My Experiences So Far</h1>
                     {/*<p className="text-gray-500">What I've Work On Before</p>*/}
                 </div>
